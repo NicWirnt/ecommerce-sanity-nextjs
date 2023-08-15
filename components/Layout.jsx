a61de229fb531d600b9406de9e97e6f106775301
NicWirnt <97414933+NicWirnt@users.noreply.github.com>
2023-08-15 16:25:09 +1000

    cart added

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,26 @@
-import React from "react";
-import Head from "next/head";
-import { Footer, Navbar } from ".";
+import React from 'react';
+import Head from 'next/head';
 
+import Navbar from './Navbar';
+import Footer from './Footer';
 
-const Layout = () => {
-  return  <div className="layout">
-    
-  </div>;
-};
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>DFD Sounds</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  )
+}
 
-export default Layout;
+export default Layout
