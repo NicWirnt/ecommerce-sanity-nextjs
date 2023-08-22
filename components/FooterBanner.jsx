f222e79e2f8acce63e68605c2852f7d2509dfd3c
NicWirnt <97414933+NicWirnt@users.noreply.github.com>
2023-08-22 14:43:21 +1000

    downgrade to next 12

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -4,15 +4,28 @@ import Image from "next/image";
 
 import { urlFor } from "@/lib/client";
 
-const FooterBanner = ({footerBanner : {discount, largeText1, largeText2, saleTime, smallText, midText,desc, product, buttonText, image}}) => {
-  
-  return <div className="footer-banner-container">
-    <div className="banner-desc">
+const FooterBanner = ({
+  footerBanner: {
+    discount,
+    largeText1,
+    largeText2,
+    saleTime,
+    smallText,
+    midText,
+    desc,
+    product,
+    buttonText,
+    image,
+  },
+}) => {
+  return (
+    <div className="footer-banner-container">
+      <div className="banner-desc">
         <div className="left">
-            <p>{discount}</p>
-            <h3>{largeText1}</h3>
-            <h3>{largeText2}</h3>
-            <p>{saleTime}</p>
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
         </div>
         <div className="right">
           <p>{smallText}</p>
@@ -22,9 +35,14 @@ const FooterBanner = ({footerBanner : {discount, largeText1, largeText2, saleTim
             <button type="button">{buttonText}</button>
           </Link>
         </div>
-        <Image src={urlFor(image).url()} width={450} height={450} className="footer-banner-image" alt={product}/>
+        <img
+          src={urlFor(image)}
+          className="footer-banner-image"
+          alt={product}
+        />
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default FooterBanner;
--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,35 +1,30 @@
 import Link from "next/link";
 import React from "react";
-import {urlFor} from "../lib/client"
+import { urlFor } from "../lib/client";
 import Image from "next/image";
 
 const HeroBanner = ({ heroBanner }) => {
   const imgUrl = urlFor(heroBanner.image);
- 
+
   return (
     <div className="hero-banner-container">
-      
-        <div>
-          <p className="beats-solo">{heroBanner.smallText}</p>
-          <h3>{heroBanner.midText}</h3>
-          <h1>{heroBanner.largeText1}</h1>
-        
-          <Image 
-          src={imgUrl.width(300).height(300).url()} width={300} height={300}
-          className="hero-banner-image"
-          alt="headphones"
-          />
+      <div>
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
 
-          <div>
-            <Link href={`/product/${heroBanner.product}`}>
-              <button type="button">{heroBanner.buttonText}</button>
-            </Link>
-            <div className="desc">
-              <h5>Description</h5>
-              <p>{heroBanner.desc}</p>
-            </div>
+        <img src={imgUrl} className="hero-banner-image" alt="headphones" />
+
+        <div>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
+      </div>
     </div>
   );
 };
